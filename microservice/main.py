df55697dc2ac102eaff14b59a608b1b2537279b3
Pavel Klapatsiuk <pavel.klapatsiuk@instinctools.com>
2024-08-25 14:07:24 +0300

    Adds docker deployment support

--- microservice/main.py
+++ microservice/main.py
@@ -56,6 +56,8 @@ async def receive_message(text: TextMessage):
 
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
+    
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8001, log_level=logging.INFO)
+    uvicorn.run(app, host="0.0.0.0", port=8001, log_level=logging.INFO)
